d4b29c9af03349ad7b16b60727fc8ec3ffb9e43f
RamezCh <ramiz_chreide@outlook.com>
2024-07-06 23:23:36 +0300

    Open Functionality

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 'use strict';
 
 // Element selection
-const showModalBtn = document.querySelector('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+// This will return the first element with the class name
+// const showModalBtn = document.querySelector('.show-modal');
+// This will return all elements with the class name in a NodeList
+const showModalBtns = document.querySelectorAll('.show-modal');
+// As If we targeted each button individually
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
