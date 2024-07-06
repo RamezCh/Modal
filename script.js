ee11ebc40c847d3b5bfcf57120f96b6958d6a3ed
RamezCh <ramiz_chreide@outlook.com>
2024-07-06 23:11:10 +0300

    Element Selection

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// Element selection
+const showModalBtn = document.querySelector('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
